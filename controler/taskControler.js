6f5484a4e098b364771803cab1013ba83abadc4e
chankaran2996 <chandrasekaran2996@gmail.com>
2025-04-08 23:05:06 +0530

    1.5

--- controler/taskControler.js
+++ controler/taskControler.js
@@ -1,18 +1,99 @@
 import Task from "../models/TaskModel.js";
 
+// getting all tasks
 export const getTask = async(req, res) => {
     try {
+        const {status} = req.query;
         
+        let fliter={};        
+
+        if(status){
+            fliter.status = status;
+        }
+
+        let tasks;
+
+        req.user.role === "admin" ? 
+        tasks = await Task.find(fliter).populate(
+            "assignedTo", 
+            "name email profileImgUrl"
+        ) 
+        : 
+        tasks = await Task.find({...fliter, assignedTo: req.user._id}).populate(
+            "assignedTo", 
+            "name email profileImgUrl"
+        );
+
+        tasks = await Promise.all(
+            tasks.map(async (task) => {
+                const completedCount = task.todoCheckList.filter((item) => item.completed).length;
+
+                return {
+                    ...task._doc,
+                    completedCount: completedCount,
+                    totalCount: task.todoCheckList.length,
+                };
+            }
+
+            )
+        );
+
+        const allTask = await Task.countDocuments(
+            req.user.role === "admin" ? {} : { assignedTo: req.user._id }
+        );
+
+        const createdTask = await Task.countDocuments({
+            status: "created",
+            ...(req.user.role === "admin" ? {} : { assignedTo: req.user._id }),
+        });
+
+        const pendingTask = await Task.countDocuments({
+            status: "pending",
+            ...(req.user.role === "admin" ? {} : { assignedTo: req.user._id }),
+        });
+
+        const inProgressTask = await Task.countDocuments({
+            status: "In progress",
+            ...(req.user.role === "admin" ? {} : { assignedTo: req.user._id }),
+        });
+
+        const completedTask = await Task.countDocuments({
+            status: "complected",
+            ...(req.user.role === "admin" ? {} : { assignedTo: req.user._id }),
+        });
+
+        res.status(200).json({
+            message: "Task fetched successfully",
+            tasks,
+            statusSummary:{
+            allTask,
+            createdTask,
+            pendingTask,
+            inProgressTask,
+            completedTask
+        }
+        }); 
+
     } catch (error) {
-        res.status(500).josn({ message: 'Serever error', error: error.message });
+        res.status(500).json({ message: 'Serever error', error: error.message });
     }
 }
 
+// getting task by id
 export const getTaskById = async(req, res) => {
     try {
-        
+        const task = await Task.findById(req.params.id).populate(
+            "assignedTo", 
+            "name email profileImgUrl"
+        );
+
+        if(!task){
+            return res.status(404).json({message:"Task not found"})
+        }
+
+        res.status(200).json({message:"Task fetched by ID successfully", task});
     } catch (error) {
-        res.status(500).josn({ message: 'Serever error', error: error.message });
+        res.status(500).json({ message: 'Serever error', error: error.message });
     }
 }
 
@@ -42,31 +123,83 @@ export const createTask = async(req, res) => {
 
         res.status(201).json({message:"Task created successfully" , task})
     } catch (error) {
-        res.status(500).josn({ message: 'Serever error', error: error.message });
+        res.status(500).json({ message: 'Serever error', error: error.message });
     }
 }
 
+// updating task
 export const updateTask = async(req, res) => {
     try {
-        
+        const task = await Task.findById(req.params.id);
+
+        if(!task){
+            return res.status(404).json({message:"Task not found"})
+        }
+
+        task.title = req.body.title || task.title;
+        task.description = req.body.description || task.description;
+        task.priority = req.body.priority || task.priority;
+        task.dueDate = req.body.dueDate || task.dueDate;
+        task.todoCheckList = req.body.todoCheckList || task.todoCheckList;
+        task.attachments = req.body.attachments || task.attachments;
+        task.assignedTo = req.body.assignedTo || task.assignedTo;
+
+        const updatedTask = await task.save();
+
+        res.status(200).json({message:"Task updated successfully", updatedTask});
     } catch (error) {
-        res.status(500).josn({ message: 'Serever error', error: error.message });
+        res.status(500).json({ message: 'Serever error', error: error.message });
     }
 }
 
+// deleting task
 export const deleteTask = async(req, res) => {
     try {
-        
+        const task = await Task.findById(req.params.id);
+
+        if(!task){
+            return res.status(404).json({message:"Task not found"})
+        }
+
+        await task.remove();
+
+        res.status(200).json({message:"Task deleted successfully"});
     } catch (error) {
-        res.status(500).josn({ message: 'Serever error', error: error.message });
+        res.status(500).json({ message: 'Serever error', error: error.message });
     }
 }
 
+// updating task status by ID
 export const updateTaskStatus = async(req, res) => {
     try {
-        
+        const task = await Task.findById(req.params.id);
+
+        if(!task){
+            return res.status(404).json({message:"Task not found"})
+        }
+
+        const isAssginedTo = task.assignedTo.some(
+            (user) => user.toString() === req.user._id.toString()
+        );
+
+        if(!isAssginedTo && req.user.role !== "admin"){
+            return res.status(403).json({message:"You are not authorized to update this task"})
+        }
+
+        task.status = req.body.status || task.status;
+
+        if(task.status === "complected"){
+            task.todoCheckList.forEach((item) => {
+                item.completed = true;
+            });
+            task.progress = 100;
+        }
+
+        await task.save();
+
+        res.status(200).json({message:"Task status updated successfully", task});
     } catch (error) {
-        res.status(500).josn({ message: 'Serever error', error: error.message });
+        res.status(500).json({ message: 'Serever error', error: error.message });
     }
 }
 
